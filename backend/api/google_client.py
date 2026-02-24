704b368f4f7407b1e67118f5a6fb72d393a2b968
justint04 <112271059+justint04@users.noreply.github.com>
2026-02-24 14:31:42 -0500

    fix google client to get formatted data, get api key from env file

--- backend/api/google_client.py
+++ backend/api/google_client.py
@@ -1,24 +1,44 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def get_place_info(address, api_key) :
     #Base URL
-    base_url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=<address_input>&inputtype=textquery&fields=formatted_address%2Cname%2Cbusiness_status%2Cplace_id&key=<api_key>"
+    base_url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
+
     params = {
         "input": address,
         "inputtype": "textquery", 
-        "fields": "formatted_address, name, business_status, place_id",
+        "fields": "formatted_address,name,business_status,place_id",
         "key": api_key, 
     }
     #Send request and capture response
     response = requests.get(base_url, params=params)
     #Check if request was successful
-    if response.status_code == 200:
-        return response.json()
-    else:
+    if response.status_code != 200:
         return None
     
-api_key = 
-address = "219 Mulberry Street, New York, NY 10012"
+    data = response.json()
+
+    if data["status"] != "OK" or not data["candidates"]:
+        return None
+    
+    place = data["candidates"][0]
+
+    return {
+        "name": place["name"],
+        "address": place["formatted_address"],
+        "place_id":place["place_id"]
+    }
+        
+    
+api_key = os.getenv("GOOGLE_PLACES_API_KEY")
+address = "Rubys Cafe SOHO"
 place_info = get_place_info(address, api_key)
+
+
 if place_info is not None:
     print(place_info)
 else:
