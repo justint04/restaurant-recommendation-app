6b6abb857175a5e754432573de8921665517088d
Justin Tsang <justinwtsang@gmail.com>
2026-03-10 12:35:11 -0400

    added category level keywords

--- backend/processing/scorer.py
+++ backend/processing/scorer.py
@@ -1,13 +1,44 @@
-#small list of positive keywords for now, add more as we go and potentially load it in postgresql instead
-POSITIVE_KEYWORDS = {
-    "delicious", "amazing", "great", "fresh", "friendly",
-    "cozy", "perfect", "loved", "excellent", "tasty"
-}
-#small list of negative keywords for now, add more as we go and potentially load it in postgresql instead
+#list of positive and negative keywords based on category
+#can already see issues with this as someone can put "not worth" and it may be scored positively, will have to fix this later
 
-NEGATIVE_KEYWORDS = {
-    "bad", "awful", "rude", "slow", "disgusting",
-    "overpriced", "cold", "wrong", "disappointed", "hostile"
+CATEGORY_KEYWORDS = {
+    "food": {
+        #positive 
+        "delicious": 1, "tasty": 1, "fresh": 1, "amazing": 1, "flavorful": 1, "perfect": 1,
+        "yummy": 1, "cooked": 1, "crispy": 1, "juicy": 1,
+        #negative
+        "nasty": -1, "bland": -1, "cold": -1, "stale": -1, "overcooked": -1, "undercooked": -1,
+        "soggy": -1, "awful": -1, "disgusting": -1, "sick": -1,
+    },
+    
+    "service": {
+        #positive
+        "friendly": 1, "attentive": 1, "fast": 1, "helpful": 1, "welcoming": 1, "professional": 1,
+        "kind": 1, "efficient": 1, "courteous": 1,
+
+        #negative
+        "rude": -1, "slow": -1, "hostile": -1, "ignored": -1, "attitude": -1, "unfriendly": -1,
+        "inattentive": -1, "unprofessional": -1,
+    },
+
+    "ambiance": {
+        #positive
+        "cozy": 1, "vibe": 1, "atmosphere": 1, "beautiful": 1, "charming": 1, "clean": 1,
+        "comfortable": 1, "cute": 1, "lovely": 1, "aesthetic": 1,
+
+        #negative
+        "loud": -1, "crowded": -1, "dirty": -1, "cramped": -1, "noisy": -1, "uncomfortable": -1,
+        "chaotic": -1, "dark": -1,
+    },
+
+    "value": {
+        #positive
+        "worth": 1, "affordable": 1, "reasonable": 1, "generous": 1, "deal": 1, "cheap": 1, "bargain": 1,
+
+        #negative
+        "overpriced": -1, "expensive": -1, "pricey": -1, "charged": -1, "steep": -1, "costly": -1,
+    }
+
 }
 
 #words is an array of relevant keywords from text_processor 
